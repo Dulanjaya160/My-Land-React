a9b76c984cf74a246e8b5a2691653e7beb60ba31
MadurangaDev <shehan@teachmeit.lk>
2025-02-01 00:32:09 +0530

    Initial codebase setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import HomePage from "./Pages/HomePage"; 
-import "./assets/CSS/index.css";
+import { RouterProvider } from "react-router-dom";
 
+import { router } from "@config/";
+import "@styles/index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HomePage/>
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "src/components/common"),
+      "@atoms": path.resolve(__dirname, "src/components/atoms"),
+      "@layouts": path.resolve(__dirname, "src/components/layouts"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@config": path.resolve(__dirname, "src/config"),
+      "@styles": path.resolve(__dirname, "src/assets/css"),
+      "@images": path.resolve(__dirname, "src/assets/images"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@redux": path.resolve(__dirname, "src/redux"),
+      "@reduxActions": path.resolve(__dirname, "src/redux/actions"),
+      "@reduxSlices": path.resolve(__dirname, "src/redux/slices"),
+      "@enums": path.resolve(__dirname, "src/typings/enums"),
+      "@interfaces": path.resolve(__dirname, "src/typings/interfaces"),
+      "@responses": path.resolve(__dirname, "src/typings/interfaces/responses"),
+      "@requests": path.resolve(__dirname, "src/typings/interfaces/requests"),
+      "@providers": path.resolve(__dirname, "src/providers"),
+      "@data": path.resolve(__dirname, "src/data"),
+      "@services": path.resolve(__dirname, "src/services"),
+    },
+  },
+});
